3b1fb9c0a02737b26a10610d98ecb308ff8d8d34
Chulshin Kim <ckim349@aucklanduni.ac.nz>
2023-05-24 13:12:36 +1200

    added dates of the week for schedule and changed weather API to meteosource

--- src/components/SchedulePage.jsx
+++ src/components/SchedulePage.jsx
@@ -16,9 +16,23 @@ function SchedulePage() {
   };
 
   function getDate() {
-    var currentDate = new Date().toLocaleDateString();
+    var currentDate = new Date().toLocaleString("en-US", {weekday: "long"});
     console.log(currentDate);
+
+
   }
+
+  //calculate days of the week with dates
+  const day = new Date();
+  const monday = day.getDate() - (day.getDay() + 6) % 7;
+  const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  var d = [];
+  for (let i = 0; i < 7; i++) {
+    d.push(monday + i);
+  }
+  const datesOfTheWeek = d.map(function (value, index) {
+    return <h3 className="schedule-days" key={value}>{days[index]} {value}</h3>
+  })
 
 
   return (
@@ -38,8 +52,10 @@ function SchedulePage() {
       </form>
       <button onClick={getDate}>Get date</button>
       <h1>Schedule Page</h1>
-      {location ? <Weather location={location} /> : null}
-      {/* <h1>Monday</h1>  */}
+      <Weather />
+      <div className="schedule-dates">
+        {datesOfTheWeek}
+      </div>
 
     </>
   )
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,18 +1,38 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-function Weather({ location }) {
+function Weather() {
   const [weatherData, setWeatherData] = useState([]);
+  const [location, setLocation] = useState({latitude: 0, longitude: 0});
+  
+  useEffect(() => {
+    {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          setLocation({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          });
+        },
+      );
+      console.log(location);
+    }
+  }, []);
+
 
   const options = {
     method: 'GET',
-    url: 'https://weatherapi-com.p.rapidapi.com/current.json',
-    url: 'https://weatherapi-com.p.rapidapi.com/forecast.json',
-    params: {q: `${location}`, days: 8},
+    url: 'https://ai-weather-by-meteosource.p.rapidapi.com/daily',
+    params: {
+      lat: `${location.latitude}`,
+      lon: `${location.longitude}`,
+      language: 'en',
+      units: 'auto'
+    },
     headers: {
       'X-RapidAPI-Key': import.meta.env.VITE_WEATHER_KEY,
-      'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-    },
+      'X-RapidAPI-Host': 'ai-weather-by-meteosource.p.rapidapi.com'
+    }
   };
 
   useEffect(() => {
@@ -27,17 +47,17 @@ function Weather({ location }) {
     };
     getWeatherData();
   }, []);
-  
-  function handleClick() {
-    console.log(weatherData.forecast.forecastday[1].day.condition.text);
-    console.log(weatherData.forecast.forecastday[2].day.condition.text);
-    console.log(weatherData.location.name);
-    console.log(weatherData.current.condition.text);
-  }
+
 
 
   return (
-    <button onClick={handleClick}>check weather :3</button>
+    <>
+      <h1>{console.log(weatherData.daily)}</h1>
+      <h1>{console.log(location)}</h1>
+
+    </>
+    
+
   )
 }
 
